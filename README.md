48fe65856c2a6728c73efbebb027f4b96a7bfca7
value-urano <urano@value-it.co.jp>
2023-12-22 15:48:47 +0900

    初回コミット

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # value-node-express-practice
+
+# Node.js + Express でWEBサーバーを起動する
+
+## ローカル起動
+```shell
+cd express
+docker compose up --build
+```
+
+## アクセス
+http://localhost:8080/
+
+---
+
+# Expressのデフォルトの同時アクセス性能を確認する
+### TBD
---
+++ express/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+
+const server = app.listen(8000, function () {
+    console.log('🚀 app started. port:' + server.address().port)
+})
+
+/**
+ * GET /
+ */
+app.get('/', async function (req, res, next) {
+    console.log('/index called')
+    res.send('/index called')
+})
+
+/**
+ * GET /sleep5
+ * ただ10秒待ってレスポンス
+ */
+app.get('/sleep5', async function (req, res, next) {
+    console.log('/sleep5 start')
+    await new Promise(resolve => setTimeout(resolve, 10000))
+    console.log('/sleep5 finish')
+    res.send('/sleep5 done')
+})
